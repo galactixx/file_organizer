33423cbe7950d40631a33c6df98fc7541c115331
galactixx <nwlezien.github@gmail.com>
2023-10-03 16:45:30 -0500

    add truncation of paths

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import shutil
 import click
 import os
 
+NUM_FOLDERS = 3
+
 def _truncate_path(path: str) -> str:
-    pass
+    """Truncate path to shorten length of string in rich output."""
+    path = os.path.normpath(path)
+    path_split = path.split(os.sep)
+    if len(path_split) <= NUM_FOLDERS:
+        return path
+    else:
+        return os.path.join(*path_split[-NUM_FOLDERS:])
 
 @click.command()
 @click.option('--path', type=click.Path(exists=True), prompt='Enter the path', help='The name of the path')
 def _path_organizer(path: str) -> None:
+    """CLI for organizing files in a given directory by file type."""
 
     # instance of rich table & console
     console = Console()
-    table = Table(title='files that were organized')
-    table.add_column('file_type', style='cyan')
-    table.add_column('source_path', style='magenta')
-    table.add_column('destination_path', style='green')
+    table = Table(title='Files that were Organized')
+    table.add_column('File Type', style='cyan')
+    table.add_column('Source Path', style='magenta')
+    table.add_column('Destination Path', style='green')
 
     contents = [i for i in os.listdir(path) if not os.path.isdir(os.path.join(path, i))]
     if not contents:
@@ -35,7 +44,9 @@ def _path_organizer(path: str) -> None:
             # make directory and move file
             os.makedirs(destination_path_main, exist_ok=True)
             shutil.move(source_path, destination_path)
-            table.add_row(file_type, source_path, destination_path)
+            table.add_row(file_type,
+                          _truncate_path(path=source_path),
+                          _truncate_path(path=destination_path))
 
         # print table in console
         console.print(table)
